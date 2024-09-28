85063462ef914a15ca2d614be110654b3944752e
Petter Ekedahl <petter.ekedahl@avensia.com>
2024-09-28 17:45:40 +0200

    feedback on send

--- src/app/components/Generator.tsx
+++ src/app/components/Generator.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const baseUrl = "http://localhost:3000";
 
 const Generator = () => {
   const [prompt, setPrompt] = useState<string>('');
+  const [promptSent, setPromptSent] = useState<boolean>(false);
+  const [noPrompt, setNoPrompt] = useState<boolean>(false);
 
   async function customGenerateAudio(prompt?: string) {
     const url = `${baseUrl}/api/generate`;
@@ -23,15 +25,31 @@ const Generator = () => {
   }
 
   const onClick = async () => {
+    if (prompt === '') {
+      setNoPrompt(true);
+      return;
+    }
     await customGenerateAudio(prompt);
+    setPromptSent(true);
+    setPrompt('');
   };
 
   const onInputChange = (value: string) => {
-    setTimeout(() => {
-      setPrompt(value);
-    }, 1000)
-    
+    setPrompt(value);
   };
+
+  useEffect(() => {
+    if (promptSent) {
+      setTimeout(() => {
+        setPromptSent(false);
+      }, 3000);
+    }
+    if (noPrompt) {
+      setTimeout(() => {
+        setNoPrompt(false);
+      }, 3000)
+    }
+  }, [promptSent, noPrompt]);
 
   return (
     <>
@@ -39,9 +57,17 @@ const Generator = () => {
         <h1 style={{ fontSize: 32 }}>Generate a song</h1>
         <div>
           <label>Prompt a song: </label>
-          <input style={{border: '1px solid black', padding: 5, borderRadius: 10}} onChange={e => onInputChange(e.target.value)} />
-          <button style={{ backgroundColor: '#092139', color: 'white', margin: '10px', padding: 5, borderRadius: 10 }} onClick={onClick}>Send</button>
+          <input style={{border: '1px solid black', padding: 5, borderRadius: 10}} onChange={e => onInputChange(e.target.value)} value={prompt} />
+          <button 
+            style={{ backgroundColor: promptSent ? 'darkgreen' : '#092139', color: 'white', margin: '10px', padding: 5, borderRadius: 10, width: 70 }} 
+            onClick={onClick}
+          >
+             {promptSent ? '✔' : 'Send'}
+          </button>
         </div>
+        {
+          noPrompt && <div style={{color: 'red'}}>A prompt is needed</div>
+        }
       </div>
     </>
   );
