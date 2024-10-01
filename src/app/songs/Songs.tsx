7a7b6a0a2483ac24bdaca414302ec44492db3feb
Anton Thelandersson <anton.thelandersson@avensia.com>
2024-10-01 14:05:07 +0200

    Add songs

---
+++ src/app/songs/Songs.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+//const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://avensia-radio-station-20.vercel.app';
+
+export interface Song {
+  id: string;
+  title: string;
+  image_url: string;
+  lyric: string;
+  audio_url: string;
+  video_url: string;
+  created_at: string;
+  model_name: string;
+  status: string;
+  gpt_description_prompt: string;
+  prompt: string;
+  type: string;
+  tags: string;
+}
+
+const SongsList: React.FC = () => {
+  const [songs, setSongs] = useState<Song[]>([]);
+
+  useEffect(() => {
+    fetchAllSongs();
+  }, []);
+
+  async function fetchAllSongs() {
+    const url = `${baseUrl}/api/get`;
+    const test = new Request(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const req = await fetch(test).then((response) => response.json()).then(data => data);
+    setSongs(req);
+    return req;
+  }
+
+  return (
+    <div>
+      <h1>Media Library</h1>
+      {songs.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <img src={item.image_url} alt={item.title} />
+          <audio controls src={item.audio_url}></audio>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SongsList;
---
+++ src/app/songs/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import SongsList from './Songs';
+
+
+export default function Songs() {
+    return (
+        <>
+            <SongsList></SongsList>
+        </>
+
+    );
+}
